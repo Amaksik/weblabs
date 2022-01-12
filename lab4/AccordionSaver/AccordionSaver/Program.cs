0c7353e704c7ab45c894284fd6b764e388b9fed9
Amaksik <a.maksik2013@gmail.com>
2022-01-12 11:31:33 +0200

    add all labs on web design

---
+++ lab4/AccordionSaver/AccordionSaver/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Net.Mime;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
+Console.WriteLine("Hello, World!");
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+app.Use(async (context, next) =>
+{
+
+    context.Response.OnStarting(() =>
+    {
+        context.Request.Headers.ContentType = "application/json";
+        context.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+        context.Response.Headers.Add("Access-Control-Allow-Methods", "POST,GET");
+        return Task.FromResult(0);
+    });
+
+    await next();
+});
+
+app.Use(async (context, next) =>
+{
+
+    context.Response.OnCompleted(() =>
+    {
+        context.Request.Headers.ContentType = "application/json";
+        return Task.FromResult(0);
+    });
+
+    await next();
+}); 
+                    
+
+app.MapPost("/postAccordions", ([FromBody(EmptyBodyBehavior = Microsoft.AspNetCore.Mvc.ModelBinding.EmptyBodyBehavior.Allow)]Accordion[] accordions,
+                     [FromHeader(Name = "Content-Type")] string contentType)
+                     => {
+                         File.WriteAllText("File.json", JsonSerializer.Serialize(accordions));
+                         return Results.Ok(accordions);
+                     });
+
+app.MapGet("/getAccordions", () =>
+{
+    var objString = File.ReadAllText("File.json");
+    var obj = JsonSerializer.Deserialize<Accordion[]>(objString);
+    return Results.Ok(obj);
+});
+app.Run();
+
+class Accordion
+{
+    public string Title { get; set; }
+    public string Content { get; set; }
+}
+// See https://aka.ms/new-console-template for more information
+
